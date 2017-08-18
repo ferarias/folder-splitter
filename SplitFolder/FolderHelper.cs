456f43b00e15478f449d0afda05b5d104be75d2a
Fernando Arias Marqués <fernando.arias@logitravelgroup.com>
2017-08-18 12:12:13 +0200

    First commit

---
+++ SplitFolder/FolderHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SplitFolder
+{
+    public static class FolderHelpr
+    {
+        /// <summary>
+        /// Split a folder in two, given the new folder name and the items that will move
+        /// </summary>
+        /// <param name="originalFolderPath">Original folder full path</param>
+        /// <param name="newFolderName">Name of the new folder. It will be created beside the original folder.</param>
+        /// <param name="items">Items' names. These items will end up into the newly created folder.</param>
+        /// <returns>
+        ///     The full path of the new folder is returned
+        ///     If nothing is moved (i.e.: no files are splitted), the original folder full path is returned
+        /// </returns>
+        public static string SplitFolder(string originalFolderPath, string newFolderName, IEnumerable<string> items)
+        {
+            if(!items.Any())
+                return originalFolderPath;
+
+            var originalDirectoryInfo = new DirectoryInfo(originalFolderPath);
+            var newFolderPath = Path.Combine(originalDirectoryInfo.Parent.FullName, newFolderName);
+            var newDirectoryInfo = Directory.CreateDirectory(newFolderPath);
+
+            foreach (var entry in items)
+            {
+                var sourceFullPath = Path.Combine(originalFolderPath, entry);
+                if(!File.Exists(sourceFullPath) && !Directory.Exists(sourceFullPath))
+                    continue;
+                var destFullPath = Path.Combine(newFolderPath, entry);
+                Directory.Move(sourceFullPath, destFullPath);
+            }
+
+            return newDirectoryInfo.FullName;
+        }
+
+    }
+}
---
+++ XUnitTestProject/SplitFolderTests.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SplitFolder;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class SplitFolderTests
+    {
+        string BasePath = Path.Combine(Path.GetTempPath(), "SPLIT_TESTS");
+
+        public SplitFolderTests()
+        {
+            Cleanup(BasePath);
+        }
+
+
+
+
+        [Theory]
+        [InlineData(0,0)]
+        [InlineData(1,0)]
+        [InlineData(5,0)]
+        [InlineData(10,0)]
+        [InlineData(100,0)]
+        [InlineData(0,1)]
+        [InlineData(0,5)]
+        [InlineData(0,10)]
+        [InlineData(0, 100)]
+        [InlineData(5,1)]
+        [InlineData(10,5)]
+        [InlineData(100,10)]
+        [InlineData(100, 100)]        
+        public void FolderWithFiles_SplitFolder_FilesAreMoved(int fileCount, int dirCount)
+        {
+            // Arrange
+            var folderName = Path.Combine(BasePath, $"20170101. {fileCount:000}_{dirCount:000}_Test");
+            var newFolderName = "SplittedFolder";
+
+            Console.WriteLine($"Splitting {folderName} into {newFolderName}...");
+            Setup(folderName, fileCount, dirCount);
+
+            var itemsMoving = new List<string>();
+            int movingFilesCount = fileCount / 2;
+            for (var i = movingFilesCount; i < fileCount; i++)
+            {
+                itemsMoving.Add($"file_{i}.txt");
+            }
+            int movingDirsCount = dirCount / 2;
+            for (var i = movingDirsCount; i < dirCount; i++)
+            {
+                itemsMoving.Add($"dir_{i}");
+            }
+
+
+            // Act
+            Console.WriteLine($"Splitting into '{newFolderName}'...");
+            var newFolder = FolderHelpr.SplitFolder(folderName, newFolderName, itemsMoving);
+
+
+            // Assert
+            var newFolderFiles = Directory.GetFiles(newFolder);
+            var newFolderDirs = Directory.GetDirectories(newFolder);
+            foreach (var itemMoving in itemsMoving)
+            {
+                var isDir = itemMoving.StartsWith("dir");
+                if (!isDir)
+                {
+                    Assert.Contains(Path.Combine(newFolder, itemMoving), newFolderFiles);
+                    Assert.DoesNotContain(Path.Combine(folderName, itemMoving), newFolderFiles);
+                }
+                else
+                {
+                    Assert.Contains(Path.Combine(newFolder, itemMoving), newFolderDirs);
+                    Assert.DoesNotContain(Path.Combine(folderName, itemMoving), newFolderDirs);
+
+                }
+            }
+
+
+            // Cleanup
+            Directory.Delete(newFolder, true);
+            Cleanup(folderName);
+        }
+
+
+
+        private void Setup(string folder, int numberOfFiles, int numberOfDirs)
+        {
+            Cleanup(folder);
+            Directory.CreateDirectory(folder);
+
+            for (var i = 0; i < numberOfFiles; i++)
+            {
+                var filePath = Path.Combine(folder, "file_" + i + ".txt");
+                var contents = @"This is the contents of file " + i;
+                File.WriteAllText(filePath, contents);
+            }
+
+            for (var i = 0; i < numberOfDirs; i++)
+            {
+                var dirPath = Path.Combine(folder, "dir_" + i);
+                Directory.CreateDirectory(dirPath);
+            }
+        }
+
+        private void Cleanup(string folder)
+        {
+            if (Directory.Exists(folder))
+            {
+                Directory.Delete(folder, true);
+            }
+        }
+
+    }
+}
